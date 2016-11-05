44dce05a4b4c0f492a3ec6c0a800f0c1ff480f9e
railem <glathor@hotmail.de>
2016-11-05 11:52:17 +0100

    ADD: ExampleBackend JPAExample Project

---
+++ ExampleBackend/JPAExample/src/main/java/com/openlimit/ExampleApplication/ExampleBackend/JPAExample/App.java
@@ -0,0 +1,13 @@
+package com.openlimit.ExampleApplication.ExampleBackend.JPAExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
