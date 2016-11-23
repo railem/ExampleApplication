9efe409527c12e582f48ec023b8b0f51bcc2b1ac
railem <glathor@hotmail.de>
2016-11-23 09:27:25 +0100

    ADD: CLI CLient Project

---
+++ ExampleFrontend/CLIClient/src/main/java/com/openlimit/ExampleApplication/ExampleFrontend/CLIClient/CLIClient.java
@@ -0,0 +1,10 @@
+package com.openlimit.ExampleApplication.ExampleFrontend.CLIClient;
+
+public class CLIClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
